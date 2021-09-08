03b9840bd4a478ee52e028703661db4315d2106e
Laura van Helden <337797@student.fontys.nl>
2021-09-08 13:02:45 +0200

    Turning on the spot is possible :)

--- Assets/Scripts/Entity/ActionInput.cs
+++ Assets/Scripts/Entity/ActionInput.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public abstract class ActionInput : MonoBehaviour
 {
+    [SerializeField] private float turnSpeed = 0.8f;
+    bool lockedInput;
     public virtual void MoveForward() { }
     public virtual void MoveBackward() { }
-    public virtual void TurnLeft() { }
-    public virtual void TurnRight() { }
+    public virtual void TurnLeft() {
+        if (!lockedInput)
+        {
+            lockedInput = true;
+            StartCoroutine(Rotate(Vector3.up * -90, turnSpeed));
+        }
+    }
+    public virtual void TurnRight() {
+        if (!lockedInput)
+        {
+            lockedInput = true;
+            StartCoroutine(Rotate(Vector3.up * 90, turnSpeed));
+        }
+    }
     public virtual void StrafeLeft() { }
     public virtual void StrafeRight() { }
     public virtual void Attack() { }
     public virtual void AlternateAttack() { }
     public virtual void Wait() { }
+
+    private IEnumerator Rotate(Vector3 angle, float time)
+    {
+        var fromAngle = transform.rotation;
+        var toAngle = Quaternion.Euler(transform.eulerAngles + angle);
+        for (var t = 0f; t <= 1; t += Time.deltaTime / time)
+        {
+            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            yield return null;
+        }
+        transform.rotation = toAngle;
+        lockedInput = false;
+    }
 }
