53c7a5e9f71ff514be0237ce4f7f95bb1a39d3eb
Laura van Helden <337797@student.fontys.nl>
2021-09-08 12:34:42 +0200

    Hooked up InputSystem. Still need to find a way to unsubscribe from lambda events though :(

---
+++ Assets/Scripts/Entity/ActionInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ActionInput : MonoBehaviour
+{
+    public abstract void MoveForward();
+    public abstract void MoveBackward();
+    public abstract void TurnLeft();
+    public abstract void TurnRight();
+    public abstract void StrafeLeft();
+    public abstract void StrafeRight();
+    public abstract void Attack();
+    public abstract void AlternateAttack();
+    public abstract void Wait();
+    public abstract void Interact();
+}
---
+++ Assets/Scripts/Entity/Player/PlayerActionInput.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerActionInput : ActionInput
+{
+    private PlayerInput playerInput;
+
+    private InputAction moveForward;
+    private InputAction moveBackward;
+    private InputAction turnLeft;
+    private InputAction turnRight;
+    private InputAction strafeLeft;
+    private InputAction strafeRight;
+    private InputAction attack;
+    private InputAction alternateAttack;
+    private InputAction wait;
+    private InputAction interact;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+
+        moveForward = playerInput.actions["MoveForward"];
+        moveBackward = playerInput.actions["MoveBackward"];
+        turnLeft = playerInput.actions["TurnLeft"];
+        turnRight = playerInput.actions["TurnRight"];
+        strafeLeft = playerInput.actions["StrafeLeft"];
+        strafeRight = playerInput.actions["StrafeRight"];
+        attack = playerInput.actions["Attack"];
+        alternateAttack = playerInput.actions["AlternateAttack"];
+        wait = playerInput.actions["Wait"];
+        interact = playerInput.actions["Interact"];
+    }
+
+    private void OnEnable()
+    {
+        moveForward.performed += _ => MoveForward();
+        moveBackward.performed += _ => MoveBackward();
+        turnLeft.performed += _ => TurnLeft();
+        turnRight.performed += _ => TurnRight();
+        strafeLeft.performed += _ => StrafeLeft();
+        strafeRight.performed += _ => StrafeRight();
+        attack.performed += _ => Attack();
+        alternateAttack.performed += _ => AlternateAttack();
+        wait.performed += _ => Wait();
+        interact.performed += _ => Interact();
+    }
+    private void OnDisable()
+    {
+
+    }
+
+    public override void AlternateAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void MoveBackward()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void MoveForward()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void StrafeLeft()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void StrafeRight()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void TurnLeft()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void TurnRight()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Wait()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Interact()
+    {
+        throw new System.NotImplementedException();
+    }
+}
