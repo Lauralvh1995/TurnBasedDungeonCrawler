1bcebc16aa0a057a5a17697fb6ab231188336d06
Laura van Helden <337797@student.fontys.nl>
2021-09-08 20:26:08 +0200

    Well, implementing A* isn't going to be easy

--- Assets/Scripts/Grid/PathFinding.cs
+++ Assets/Scripts/Grid/PathFinding.cs
@@ -9,13 +9,6 @@ namespace Assets.Scripts.Grid
 {
     public class PathFinding
     {
-
-
-        Grid<NavigationTile> navigationGrid;
-
-        public PathFinding(int width, int length, int height)
-        {
-            navigationGrid = new Grid<NavigationTile>(width * 3, height * 3, length * 3, -Vector3.one * (1f / 3f), 1f / 3f);
-        }
+        
     }
 }
