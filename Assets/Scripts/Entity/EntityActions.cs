4c14c840c3f0654a22a4e241d72bd210bf2089a5
Laura van Helden <337797@student.fontys.nl>
2021-09-09 10:52:44 +0200

    Rewrote player/entity actions to work via composition instead of inheritance

--- Assets/Scripts/Entity/EntityActions.cs
+++ Assets/Scripts/Entity/EntityActions.cs
@@ -3,53 +3,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class ActionInput : MonoBehaviour
+[RequireComponent(typeof(Entity))]
+public class EntityActions : MonoBehaviour
 {
+    [SerializeField] private Entity entity;
+
     [SerializeField] private GridController grid;
     [SerializeField] private float turnSpeed = 0.8f;
     bool lockedInput;
 
-    public virtual void Awake()
+    public void Awake()
     {
+        entity = GetComponent<Entity>();
         if(grid == null)
             grid = FindObjectOfType<GridController>();
     }
-    public virtual void MoveForward() {
+    public void MoveForward() {
         if (!lockedInput)
         {
             lockedInput = true;
             StartCoroutine(Move(Vector3.forward, turnSpeed));
         }
     }
-    public virtual void MoveBackward() {
+    public void MoveBackward() {
         if (!lockedInput)
         {
             lockedInput = true;
             StartCoroutine(Move(Vector3.back, turnSpeed));
         }
     }
-    public virtual void TurnLeft() {
+    public void TurnLeft() {
         if (!lockedInput)
         {
             lockedInput = true;
             StartCoroutine(Rotate(Vector3.up * -90, turnSpeed));
         }
     }
-    public virtual void TurnRight() {
+    public void TurnRight() {
         if (!lockedInput)
         {
             lockedInput = true;
             StartCoroutine(Rotate(Vector3.up * 90, turnSpeed));
         }
     }
-    public virtual void StrafeLeft() {
+    public void StrafeLeft() {
         if (!lockedInput)
         {
             lockedInput = true;
             StartCoroutine(Move(Vector3.left, turnSpeed));
         }
     }
-    public virtual void StrafeRight() {
+    public void StrafeRight() {
         if (!lockedInput)
         {
             lockedInput = true;
@@ -59,6 +63,7 @@ public abstract class ActionInput : MonoBehaviour
     public void Attack() { }
     public void AlternateAttack() { }
     public void Wait() { }
+    public void Interact() { }
 
     private IEnumerator Rotate(Vector3 angle, float time)
     {
---
+++ Assets/Scripts/Entity/Player/PlayerActions.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(EntityActions))]
+public class PlayerActions : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private EntityActions actions;
+
+    private InputAction moveForward;
+    private InputAction moveBackward;
+    private InputAction turnLeft;
+    private InputAction turnRight;
+    private InputAction strafeLeft;
+    private InputAction strafeRight;
+    private InputAction attack;
+    private InputAction alternateAttack;
+    private InputAction wait;
+    private InputAction interact;
+
+    public void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        actions = GetComponent<EntityActions>();
+
+        moveForward = playerInput.actions["MoveForward"];
+        moveBackward = playerInput.actions["MoveBackward"];
+        turnLeft = playerInput.actions["TurnLeft"];
+        turnRight = playerInput.actions["TurnRight"];
+        strafeLeft = playerInput.actions["StrafeLeft"];
+        strafeRight = playerInput.actions["StrafeRight"];
+        attack = playerInput.actions["Attack"];
+        alternateAttack = playerInput.actions["AlternateAttack"];
+        wait = playerInput.actions["Wait"];
+        interact = playerInput.actions["Interact"];
+    }
+
+    private void OnEnable()
+    {
+        moveForward.performed += _ => actions.MoveForward();
+        moveBackward.performed += _ => actions.MoveBackward();
+        turnLeft.performed += _ => actions.TurnLeft();
+        turnRight.performed += _ => actions.TurnRight();
+        strafeLeft.performed += _ => actions.StrafeLeft();
+        strafeRight.performed += _ => actions.StrafeRight();
+        attack.performed += _ => actions.Attack();
+        alternateAttack.performed += _ => actions.AlternateAttack();
+        wait.performed += _ => actions.Wait();
+        interact.performed += _ => actions.Interact();
+    }
+    private void OnDisable()
+    {
+        
+    }
+}
