cc304aa57a5c6f1fd86e3b2e0ec568db98129612
Laura van Helden <337797@student.fontys.nl>
2021-09-13 11:32:44 +0200

    Made simple doors also toggleable now

--- Assets/Scripts/Entity/Interactables/Toggleable.cs
+++ Assets/Scripts/Entity/Interactables/Toggleable.cs
@@ -6,13 +6,15 @@ public class Toggleable : Interactable
 {
     [SerializeField] private bool on;
     [SerializeField] private bool locked;
+
+    [SerializeField] private Transform fill;
     public override void Execute()
     {
         if(!locked)
             on = !on;
 
-        GetComponent<BoxCollider>().enabled = on;
-        GetComponent<MeshRenderer>().enabled = on;
+        fill.GetComponent<BoxCollider>().enabled = on;
+        fill.GetComponent<MeshRenderer>().enabled = on;
     }
 
     public override void Execute(Attack interactingAttack, Vector3 origin)
@@ -24,7 +26,7 @@ public class Toggleable : Interactable
     public void ChangeState(bool state)
     {
         this.on = state;
-        GetComponent<BoxCollider>().enabled = state;
-        GetComponent<MeshRenderer>().enabled = state;
+        fill.GetComponent<BoxCollider>().enabled = state;
+        fill.GetComponent<MeshRenderer>().enabled = state;
     }
 }
