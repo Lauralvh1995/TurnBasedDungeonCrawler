1918b4bc571231b8eee69e80b4188483a94008a5
Laura van Helden <337797@student.fontys.nl>
2021-09-15 14:18:52 +0200

    start of ai behaviour

--- Assets/Scripts/Entity/Enemy/AIType.cs
+++ Assets/Scripts/Entity/Enemy/AIType.cs
@@ -5,5 +5,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="New AI Type", menuName ="AI Type")]
 public class AIType : ScriptableObject
 {
+    public Node rootNode;
+    public Node.State treeState = Node.State.Running;
     
+    public Node.State Update()
+    {
+        if(rootNode.state == Node.State.Running)
+        {
+            treeState = rootNode.Update();
+        }
+        return treeState;
+    }
 }
---
+++ Assets/Scripts/Entity/Enemy/AITypes/BehaviourTree/CompositeNode.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CompositeNode : Node
+{
+    public List<Node> children = new List<Node>();
+}
