e8bc21d1a6fb51c853a7b886f2418c00a59e074f
Laura van Helden <337797@student.fontys.nl>
2021-09-20 09:44:32 +0200

    Made prefabs, decided I don't want a Behaviour Tree, and made a 3d visualisation of The Reservoir

--- Assets/Scripts/Entity/Enemy/AIType.cs
+++ Assets/Scripts/Entity/Enemy/AIType.cs
@@ -5,15 +5,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="New AI Type", menuName ="AI Type")]
 public class AIType : ScriptableObject
 {
-    public Node rootNode;
-    public Node.State treeState = Node.State.Running;
-    
-    public Node.State Update()
-    {
-        if(rootNode.state == Node.State.Running)
-        {
-            treeState = rootNode.Update();
-        }
-        return treeState;
-    }
+   
 }
