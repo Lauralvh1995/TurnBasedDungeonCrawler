33b6b724e49710845a1e573da13fddb9748fe189
Laura van Helden <337797@student.fontys.nl>
2021-09-22 09:59:06 +0200

    MapCamera has controls, that could be better

--- Assets/Scripts/UIControl/MapCameraController.cs
+++ Assets/Scripts/UIControl/MapCameraController.cs
@@ -1,27 +1,61 @@
-﻿using System.Collections;
+﻿using Assets.Scripts.Grid;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MapCameraController : MonoBehaviour
 {
+    [SerializeField] private float defaultZoom = 10f;
+    [SerializeField] private GridController grid;
     [SerializeField] private Player player;
     [SerializeField] private Vector3 offset;
+    [SerializeField] private Camera mapCamera;
     private void OnEnable()
     {
+        grid = FindObjectOfType<GridController>();
+        mapCamera = GetComponent<Camera>();
         ResetMap();
     }
     public void ResetMap()
     {
         transform.position = player.transform.position + offset;
+        mapCamera.orthographicSize = defaultZoom;
     }
 
-    public void GoUp()
+    public void GoUpLevel()
     {
         transform.position += Vector3.up;
     }
 
-    public void GoDown()
+    public void GoDownLevel()
     {
         transform.position += Vector3.down;
+    }
+
+    public void GoLeft()
+    {
+        transform.position += Vector3.left;
+    }
+    public void GoRight()
+    {
+        transform.position += Vector3.right;
+    }
+    public void GoUp()
+    {
+        transform.position += Vector3.forward;
+    }
+    public void GoDown()
+    {
+        transform.position += Vector3.back;
+    }
+
+    public void ZoomIn()
+    {
+        mapCamera.orthographicSize--;
+    }
+    public void ZoomOut()
+    {
+        mapCamera.orthographicSize++;
     }
 }
