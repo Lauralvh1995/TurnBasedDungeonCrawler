a7706800ee2dea2ed18d50885027c63762341040
Laura van Helden <337797@student.fontys.nl>
2021-09-23 12:22:46 +0200

    Fixed doors, I think

--- Assets/Scripts/Entity/Interactables/Listeners/Door.cs
+++ Assets/Scripts/Entity/Interactables/Listeners/Door.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Door : Listener
 {
     [SerializeField] private bool on;
+    [SerializeField] private Transform fill;
     public void Execute(Attack interactingAttack, Vector3 origin)
     {
         //TODO: write check for stuff like powerglove/destruction
@@ -15,12 +16,21 @@ public class Door : Listener
     public void ChangeState(bool state)
     {
         on = state;
+        fill.gameObject.SetActive(on);
         grid.UpdatePassability(transform.position + transform.rotation * Vector3.forward * 0.5f);
         grid.UpdatePassability(transform.position + transform.rotation * Vector3.back * 0.5f);
     }
 
     public override void Execute()
     {
-        throw new System.NotImplementedException();
+        bool check = false;
+        foreach(Condition c in conditions)
+        {
+            check = c.Check();
+        }
+        if (check)
+        {
+            ChangeState(!on);
+        }
     }
 }
--- Assets/Scripts/Entity/Interactables/Listeners/Trapdoor.cs
+++ Assets/Scripts/Entity/Interactables/Listeners/Trapdoor.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Trapdoor : Listener
 {
     [SerializeField] private bool on;
+    [SerializeField] private Transform fill;
     public void Execute(Attack interactingAttack, Vector3 origin)
     {
         //TODO: write check for stuff like powerglove/destruction
@@ -15,12 +16,21 @@ public class Trapdoor : Listener
     public void ChangeState(bool state)
     {
         on = state;
+        fill.gameObject.SetActive(on);
         Debug.Log("Trapdoor at " + transform.position + " updating");
         grid.UpdatePassability(transform.position);
     }
 
     public override void Execute()
     {
-        throw new System.NotImplementedException();
+        bool check = false;
+        foreach (Condition c in conditions)
+        {
+            check = c.Check();
+        }
+        if (check)
+        {
+            ChangeState(!on);
+        }
     }
 }
