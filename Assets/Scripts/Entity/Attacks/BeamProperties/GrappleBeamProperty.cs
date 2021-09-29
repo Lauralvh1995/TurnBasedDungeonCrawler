d71ba581b3ba6fc056012fe00fc60efda51b1180
Laura van Helden <337797@student.fontys.nl>
2021-09-29 10:02:21 +0200

    Start of making the grapple beam

---
+++ Assets/Scripts/Entity/Attacks/BeamProperties/GrappleBeamProperty.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Grapple Beam Property", menuName = "RangedAttacks/Beam Properties/Grapple Beam")]
+public class GrappleBeamProperty : BeamProperty
+{
+    public override void ExecuteBeamEffect(Vector3 location)
+    {
+        
+    }
+}
