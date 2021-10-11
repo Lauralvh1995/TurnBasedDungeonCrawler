039d94ef2091108f899a3f9956553b068be543cf
Laura van Helden <337797@student.fontys.nl>
2021-10-11 15:41:58 +0200

    Added a textbox, and an end to the game

---
+++ Assets/Scripts/Entity/Interactables/Listeners/TextPopUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TextPopUp : Listener
+{
+    [SerializeField] private ShowTextEvent showTextEvent;
+    [SerializeField] private string textToShow;
+    public override void Execute()
+    {
+        showTextEvent.Invoke(textToShow);
+    }
+}
+
+[Serializable]
+public class ShowTextEvent : UnityEvent<string>
+{
+
+}
---
+++ Assets/Scripts/UIControl/TextBoxController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextBoxController : MonoBehaviour
+{
+    [SerializeField] RectTransform textBox;
+    [SerializeField] TextMeshProUGUI uiText;
+
+    public void ShowText(string text)
+    {
+        textBox.gameObject.SetActive(true);
+        StartCoroutine(TypeWriter(text));
+        Invoke("HideText", 5f);
+    }
+
+    IEnumerator TypeWriter(string text)
+    {
+        for(int i = 0; i < text.Length; i++)
+        {
+            uiText.text += text[i];
+            yield return null;
+        }
+    }
+
+    private void HideText()
+    {
+        uiText.text = "";
+        textBox.gameObject.SetActive(false);
+    }
+}
