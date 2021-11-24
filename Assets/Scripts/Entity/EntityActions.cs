8462b0e96f86b42037ae8d74ff811105b97b27ee
Laura van Helden <337797@student.fontys.nl>
2021-11-24 15:54:45 +0100

    forgot some

--- Assets/Scripts/Entity/EntityActions.cs
+++ Assets/Scripts/Entity/EntityActions.cs
@@ -5,157 +5,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-[RequireComponent(typeof(Entity))]
-public class EntityActions : MonoBehaviour
+namespace Assets.Scripts.Entity
 {
-    [SerializeField] private Entity entity;
-    [SerializeField] private float turnSpeed = 0.1f;
-    [SerializeField] bool lockedInput;
-    [SerializeField] TurnState requiredPhase;
+    [RequireComponent(typeof(Entity))]
+    public class EntityActions : MonoBehaviour
+    {
+        [SerializeField] private Entity entity;
+        [SerializeField] private float turnSpeed = 0.1f;
+        [SerializeField] bool lockedInput;
+        [SerializeField] TurnState requiredPhase;
 
-    public void Awake()
-    {
-        entity = GetComponent<Entity>();
-    }
-    public void CheckFloor()
-    {
-        if (!GridController.Instance.GetTileFromWorldPosition(transform.position).Floor)
+        public void Awake()
         {
-            lockedInput = true;
-            if (!entity.IsFlying())
+            entity = GetComponent<Entity>();
+        }
+        public void CheckFloor()
+        {
+            if (!GridController.Instance.GetTileFromWorldPosition(transform.position).Floor)
             {
-                StartCoroutine(FallDown());
+                lockedInput = true;
+                if (!entity.IsFlying())
+                {
+                    StartCoroutine(FallDown());
+                }
             }
         }
-    }
 
-    public IEnumerator FallDown()
-    {
-        Vector3 from = transform.position;
-        Vector3 to = transform.position + Vector3.down;
+        public IEnumerator FallDown()
+        {
+            Vector3 from = transform.position;
+            Vector3 to = transform.position + Vector3.down;
 
-        for (float t = 0f; t <= 1; t += Time.deltaTime / turnSpeed)
-        {
-            transform.position = Vector3.Lerp(from, to, t);
-            yield return null;
-        }
-        transform.position = to;
-        GridController.Instance.UpdatePassability(to);
-        GridController.Instance.UpdatePassability(from);
-        entity.UpdateInteractables();
-
-        lockedInput = false;
-        CheckFloor();
-    }
-
-    public void LockInput(bool state)
-    {
-        lockedInput = state;
-    }
-
-    public void MoveForward()
-    {
-        if (!lockedInput && TurnManager.Instance.TurnState == requiredPhase)
-        {
-            lockedInput = true;
-            StartCoroutine(Move(Vector3.forward, turnSpeed));
-        }
-    }
-    public void MoveBackward()
-    {
-        if (!lockedInput && TurnManager.Instance.TurnState == requiredPhase)
-        {
-            lockedInput = true;
-            StartCoroutine(Move(Vector3.back, turnSpeed));
-        }
-    }
-    public void TurnLeft()
-    {
-        if (!lockedInput && TurnManager.Instance.TurnState == requiredPhase)
-        {
-            lockedInput = true;
-            StartCoroutine(Rotate(Vector3.up * -90, turnSpeed));
-        }
-    }
-    public void TurnRight()
-    {
-        if (!lockedInput && TurnManager.Instance.TurnState == requiredPhase)
-        {
-            lockedInput = true;
-            StartCoroutine(Rotate(Vector3.up * 90, turnSpeed));
-        }
-    }
-    public void StrafeLeft()
-    {
-        if (!lockedInput && TurnManager.Instance.TurnState == requiredPhase)
-        {
-            lockedInput = true;
-            StartCoroutine(Move(Vector3.left, turnSpeed));
-        }
-    }
-    public void StrafeRight()
-    {
-        if (!lockedInput && TurnManager.Instance.TurnState == requiredPhase)
-        {
-            lockedInput = true;
-            StartCoroutine(Move(Vector3.right, turnSpeed));
-        }
-    }
-    public void Attack()
-    {
-        if (!lockedInput && TurnManager.Instance.TurnState == requiredPhase)
-        {
-            if (!EventSystem.current.IsPointerOverGameObject() || entity is Enemy)
-                entity.ExecutePrimaryAttack();
-            PassTurn();
-        }
-    }
-    public void AlternateAttack()
-    {
-        if (!lockedInput && TurnManager.Instance.TurnState == requiredPhase)
-        {
-            if (!EventSystem.current.IsPointerOverGameObject() || entity is Enemy)
-                entity.ExecuteSecondaryAttack();
-            PassTurn();
-        }
-    }
-    public void Wait()
-    {
-        if (!lockedInput && TurnManager.Instance.TurnState == requiredPhase)
-        {
-            PassTurn();
-        }
-    }
-    public void Interact()
-    {
-        if (!lockedInput && TurnManager.Instance.TurnState == requiredPhase)
-        {
-            entity.ExecuteInteraction();
-            PassTurn();
-        }
-    }
-
-    private IEnumerator Rotate(Vector3 angle, float time)
-    {
-        var fromAngle = transform.rotation;
-        var toAngle = Quaternion.Euler(transform.eulerAngles + angle);
-        for (var t = 0f; t <= 1; t += Time.deltaTime / time)
-        {
-            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
-            yield return null;
-        }
-        transform.rotation = toAngle;
-        entity.UpdateInteractables();
-        lockedInput = false;
-    }
-
-    private IEnumerator Move(Vector3 direction, float time)
-    {
-        Vector3 from = transform.position;
-        Vector3 to = transform.position + transform.rotation * direction;
-        if (GridController.Instance.CanMoveThere(from, to, entity.IsFlying()))
-        {
-            for (float t = 0f; t <= 1; t += Time.deltaTime / time)
+            for (float t = 0f; t <= 1; t += Time.deltaTime / turnSpeed)
             {
                 transform.position = Vector3.Lerp(from, to, t);
                 yield return null;
@@ -163,17 +44,139 @@ public class EntityActions : MonoBehaviour
             transform.position = to;
             GridController.Instance.UpdatePassability(to);
             GridController.Instance.UpdatePassability(from);
-            
-            PassTurn();
+            entity.UpdateInteractables();
+
+            lockedInput = false;
+            CheckFloor();
         }
-        lockedInput = false;
+
+        public void LockInput(bool state)
+        {
+            lockedInput = state;
+        }
+
+        public void MoveForward()
+        {
+            if (!lockedInput && TurnManager.Instance.TurnState == requiredPhase)
+            {
+                lockedInput = true;
+                StartCoroutine(Move(Vector3.forward, turnSpeed));
+            }
+        }
+        public void MoveBackward()
+        {
+            if (!lockedInput && TurnManager.Instance.TurnState == requiredPhase)
+            {
+                lockedInput = true;
+                StartCoroutine(Move(Vector3.back, turnSpeed));
+            }
+        }
+        public void TurnLeft()
+        {
+            if (!lockedInput && TurnManager.Instance.TurnState == requiredPhase)
+            {
+                lockedInput = true;
+                StartCoroutine(Rotate(Vector3.up * -90, turnSpeed));
+            }
+        }
+        public void TurnRight()
+        {
+            if (!lockedInput && TurnManager.Instance.TurnState == requiredPhase)
+            {
+                lockedInput = true;
+                StartCoroutine(Rotate(Vector3.up * 90, turnSpeed));
+            }
+        }
+        public void StrafeLeft()
+        {
+            if (!lockedInput && TurnManager.Instance.TurnState == requiredPhase)
+            {
+                lockedInput = true;
+                StartCoroutine(Move(Vector3.left, turnSpeed));
+            }
+        }
+        public void StrafeRight()
+        {
+            if (!lockedInput && TurnManager.Instance.TurnState == requiredPhase)
+            {
+                lockedInput = true;
+                StartCoroutine(Move(Vector3.right, turnSpeed));
+            }
+        }
+        public void Attack()
+        {
+            if (!lockedInput && TurnManager.Instance.TurnState == requiredPhase)
+            {
+                if (!EventSystem.current.IsPointerOverGameObject() || entity is Enemy)
+                    entity.ExecutePrimaryAttack();
+                PassTurn();
+            }
+        }
+        public void AlternateAttack()
+        {
+            if (!lockedInput && TurnManager.Instance.TurnState == requiredPhase)
+            {
+                if (!EventSystem.current.IsPointerOverGameObject() || entity is Enemy)
+                    entity.ExecuteSecondaryAttack();
+                PassTurn();
+            }
+        }
+        public void Wait()
+        {
+            if (!lockedInput && TurnManager.Instance.TurnState == requiredPhase)
+            {
+                PassTurn();
+            }
+        }
+        public void Interact()
+        {
+            if (!lockedInput && TurnManager.Instance.TurnState == requiredPhase)
+            {
+                entity.ExecuteInteraction();
+                PassTurn();
+            }
+        }
+
+        private IEnumerator Rotate(Vector3 angle, float time)
+        {
+            var fromAngle = transform.rotation;
+            var toAngle = Quaternion.Euler(transform.eulerAngles + angle);
+            for (var t = 0f; t <= 1; t += Time.deltaTime / time)
+            {
+                transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+                yield return null;
+            }
+            transform.rotation = toAngle;
+            entity.UpdateInteractables();
+            lockedInput = false;
+        }
+
+        private IEnumerator Move(Vector3 direction, float time)
+        {
+            Vector3 from = transform.position;
+            Vector3 to = transform.position + transform.rotation * direction;
+            if (GridController.Instance.CanMoveThere(from, to, entity.IsFlying()))
+            {
+                for (float t = 0f; t <= 1; t += Time.deltaTime / time)
+                {
+                    transform.position = Vector3.Lerp(from, to, t);
+                    yield return null;
+                }
+                transform.position = to;
+                GridController.Instance.UpdatePassability(to);
+                GridController.Instance.UpdatePassability(from);
+
+                PassTurn();
+            }
+            lockedInput = false;
+        }
+
+        void PassTurn()
+        {
+            CheckFloor();
+            entity.UpdateInteractables();
+            TurnManager.Instance.PassTurn();
+        }
     }
 
-    void PassTurn()
-    {
-        CheckFloor();
-        entity.UpdateInteractables();
-        TurnManager.Instance.PassTurn();
-    }
 }
-
--- Assets/Scripts/Entity/EntityStats.cs
+++ Assets/Scripts/Entity/EntityStats.cs
@@ -1,48 +1,51 @@
-﻿using Assets.Scripts.Entity;
+﻿
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Entity", menuName = "Entity")]
-public class EntityStats : ScriptableObject
+namespace Assets.Scripts.Entity
 {
-    [SerializeField] private string enemyName;
-    [SerializeField] private int maxHP;
-    [SerializeField] private bool flying;
-    [SerializeField] private bool heavy;
+    [CreateAssetMenu(fileName = "New Entity", menuName = "Entity")]
+    public class EntityStats : ScriptableObject
+    {
+        [SerializeField] private string enemyName;
+        [SerializeField] private int maxHP;
+        [SerializeField] private bool flying;
+        [SerializeField] private bool heavy;
 
-    [SerializeField] Attack primaryAttack;
-    [SerializeField] Attack secondaryAttack;
-    [SerializeField] AIType aiType;
+        [SerializeField] Attack primaryAttack;
+        [SerializeField] Attack secondaryAttack;
+        [SerializeField] AIType aiType;
 
-    public string GetName()
-    {
-        return enemyName;
-    }
+        public string GetName()
+        {
+            return enemyName;
+        }
 
-    public int GetMaxHP()
-    {
-        return maxHP != 0 ? maxHP : 10;
-    }
-    public bool GetFlying()
-    {
-        return flying;
-    }
+        public int GetMaxHP()
+        {
+            return maxHP != 0 ? maxHP : 10;
+        }
+        public bool GetFlying()
+        {
+            return flying;
+        }
 
-    public void SetFlying(bool value)
-    {
-        flying = value;
-    }
-    public bool GetHeavy()
-    {
-        return heavy;
-    }
-    public Attack GetPrimaryAttack()
-    {
-        return primaryAttack;
-    }
-    public Attack GetSecondaryAttack()
-    {
-        return secondaryAttack;
+        public void SetFlying(bool value)
+        {
+            flying = value;
+        }
+        public bool GetHeavy()
+        {
+            return heavy;
+        }
+        public Attack GetPrimaryAttack()
+        {
+            return primaryAttack;
+        }
+        public Attack GetSecondaryAttack()
+        {
+            return secondaryAttack;
+        }
     }
 }
