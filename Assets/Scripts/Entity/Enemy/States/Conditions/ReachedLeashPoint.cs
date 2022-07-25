069ccbb566024b90e85da3330f545e9d2439a07b
Helden,Laura L.H.A. van <l.vanhelden@student.fontys.nl>
2022-07-25 18:24:59 +0200

    fixing the state stuff at another time

---
+++ Assets/Scripts/Entity/Enemy/States/Conditions/ReachedLeashPoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReachedLeashPoint : StateChangeCondition
+{
+    public Vector3 leashPoint;
+    public override bool ConditionMet()
+    {
+        return Vector3.Distance(transform.position, leashPoint) < 0.01f;
+    }
+}
